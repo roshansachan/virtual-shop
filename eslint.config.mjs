4345c03140df13c962138af2b17de5320d316c79
Roshan Sachan <roshan.sachan@shop101.com>
2025-09-23 21:12:08 +0530

    change esling config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,6 +20,15 @@ const eslintConfig = [
       "next-env.d.ts",
     ],
   },
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-implicit-any-catch": "off",
+      "@typescript-eslint/ban-types": "off",
+      "@typescript-eslint/no-non-null-assertion": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
